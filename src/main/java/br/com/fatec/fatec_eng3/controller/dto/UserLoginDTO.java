0671f80e11db58e10e55fc0ff9714ee46e78807e
Bruna Topan <brutopanb@gmail.com>
2024-10-08 20:44:09 -0300

    feat: add user registration and user login dto

---
+++ src/main/java/br/com/fatec/fatec_eng3/controller/dto/UserLoginDTO.java
@@ -0,0 +1,29 @@
+package br.com.fatec.fatec_eng3.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserLoginDTO {
+
+    @NotBlank(message = "O nome de usuário não pode estar em branco")
+    private String username;
+
+    @NotBlank(message = "A senha não pode estar em branco")
+    private String password;
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
